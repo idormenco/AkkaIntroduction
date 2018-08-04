90fdc57b5dcad50107585845efb39796fcce2f15
idormenco <idormenco@gmail.com>
2018-08-04 12:55:00 +0300

    first

---
+++ Akka.Net.Intro/1.Akka.Net.BasicHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.Akka.Net.BasicHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
